b06404dad527d81848ec096b27afdadad1939696
Carina Sauter <carina.sauter@berkeley.edu>
2017-12-09 00:47:39 -0800

    cleaned up mindReader.js

--- mindReader.js
+++ mindReader.js
@@ -1,11 +1,10 @@
 
 // data analysis
 
-// this reads a lifestream of data, runs it 
-// against the model and outputs result
+// this reads a livestream of data from the mindwave, runs it 
+// against the model (via child_process.exec function) and outputs result
 
 
-//  my Mindwave device using the npm library
 var Mindwave = require('mindwave');
 var mw = new Mindwave();
 const fs = require('fs');
@@ -20,13 +19,10 @@ buf = [];
 mw.on('wave', function(wave){
 	var waveData = String(wave);
 	buf.push(waveData);
-	// child_process.exec(command[, options][, callback])
 	if (buf.length >= 512) {
 		const exec = require('child_process').exec;
 		console.log("calling model.")
 		var input = JSON.stringify(buf);
-		// console.log("input is: " + input)
-		// '"/path/to/test file/test.sh" arg1 arg2'
 		var cmd = 'python mindreadingmachine.py ' + input;
 		exec(cmd, (err, stdout, stderr) => {
 			if (err) {
